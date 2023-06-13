992c4f84cdfbc688f32a5addebe626cbebe8c70d
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 12:46:45 +0530

    create the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,53 @@
-
+import { useState } from 'react';
 
 function App() {
+
+  const [form, setForm] = useState({
+    amount: 0,
+    description: " ",
+    date: " "
+});
+
+function handleInput(e) {  
+  setForm({...form, [e.target.name]: e.target.value});
+}
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(form)
+}
+
+
   return (
     <div>
-      <h1>App</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="number" 
+          name="amount"
+          value={form.amount}
+          onChange={handleInput}
+          placeholder="Enter transaction amount" 
+        />
+        <input 
+          type="text"
+          name="description"
+          value={form.description}
+          onChange={handleInput}
+          placeholder="Enter transaction detail"
+        />
+        <input 
+          type="date"  
+          name="date"
+          value={form.date}
+          onChange={handleInput}
+        />
+        <button 
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
