65476aed68f2177774bb7170e1278edb978e2087
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 17:42:06 +0530

    TransactionAPI file

---
+++ server/routes/TransactionsAPI.js
@@ -0,0 +1,24 @@
+const Router = require('express')
+const Transaction = require('../models/transaction');
+
+
+const router = Router();
+
+router.get('/', async (req, res)=>{
+    const transaction = await Transaction.find({}).sort({createdAt: -1}); //sort to new to old
+    res.json({data: transaction});
+});
+
+    //post request
+router.post('/',async (req, res)=>{
+    const {amount, description, date} = req.body;
+    const transaction = new Transaction({
+        amount,
+        description,
+        date   //since the key and variable is same no need to repeat like amount:amount...
+    });
+    await transaction.save(); //save to db
+    res.json({message: "Success"});
+});
+
+module.exports = router;
