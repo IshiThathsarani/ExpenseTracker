00e58247054136122b692d8a86e9268348f3ea06
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 11:12:01 +0530

    connect with db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = 4000;
 const app = express();
+
+// connect to mongodb
+const mongodbURL = 
+"mongodb+srv://ishini:ishini99@expense.x76dkjf.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(
+    mongodbURL
+)
+.then(()=> console.log("Mongodb connected successfully"))
+.catch(err => console.log(err));
 
 app.get('/',(req, res)=>{
     res.send("Hello World");
