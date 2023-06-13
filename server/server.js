2138f84b639ba794b7e198494cdbe90598eea39a
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 17:44:32 +0530

    connecting with transactionAPI file

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const Transaction = require('./models/transaction');
+
+const TransactionsAPI = require('./routes/TransactionsAPI');
 
 const PORT = 4000;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/transaction', TransactionsAPI);
 
 // connect to mongodb
 const mongodbURL = 
@@ -22,22 +25,6 @@ console.log("Mongodb connected successfully")
     //get request
 app.get('/',(req, res)=>{
     res.send("Hello World");
-});
-app.get('/transaction', async (req, res)=>{
-    const transaction = await Transaction.find({}).sort({createdAt: -1}); //sort to new to old
-    res.json({data: transaction});
-});
-
-    //post request
-app.post('/transaction',async (req, res)=>{
-    const {amount, description, date} = req.body;
-    const transaction = new Transaction({
-        amount,
-        description,
-        date   //since the key and variable is same no need to repeat like amount:amount...
-    });
-    await transaction.save(); //save to db
-    res.json({message: "Success"});
 });
 
 app.listen(PORT, ()=>{
