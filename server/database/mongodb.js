4a3cffbd489c822da47ce3b97d1065387c0d42de
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 17:56:09 +0530

    database connection

---
+++ server/database/mongodb.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+function connect() {  //async at the begining if want
+const mongodbURL = 
+    "mongodb+srv://ishini:ishini99@expense.x76dkjf.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect( //if async is there this should be await in the begining
+    mongodbURL
+)
+console.log("Mongodb connected successfully")
+}
+
+exports = module.exports = connect;
