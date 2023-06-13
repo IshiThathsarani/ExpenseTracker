2c2afca40917337f9bfe6c7b1089a783b455219e
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 14:02:35 +0530

    database model

---
+++ server/models/transaction.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const transactionSchema = new Schema({
+    amount: Number,
+    description: String,
+    date: { type: Date, default: new Date },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
