778a46989409773428eff5c3785d98d74563ca15
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-13 13:47:19 +0530

    post api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = 4000;
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // connect to mongodb
 const mongodbURL = 
@@ -13,9 +18,15 @@ mongoose.connect(
 )
 console.log("Mongodb connected successfully")
 
-
+    //get request
 app.get('/',(req, res)=>{
     res.send("Hello World");
+});
+
+    //post request
+app.post('/transaction',(req, res)=>{
+    const {amount, description, date} = req.body;
+    res.json({message: "Hello World"});
 });
 
 app.listen(PORT, ()=>{
