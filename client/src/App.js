b0fdd29874e1b5660c3dc6c024207792ba530c58
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-18 08:05:26 +0530

    remove  the transaction form

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,11 @@ import { useEffect, useState } from 'react';
 import AppBar from './components/AppBar';
 import TransactionForm from './components/TransactionForm';
 
-const InitialForm = {
-  amount: 0,
-  description: " ",
-  date: " "
-}
+
 
 function App() {
 
-  const [form, setForm] = useState(InitialForm);
+  
 
 const [transactions, setTransactions] = useState([]);
 
@@ -24,60 +20,16 @@ async function fetchTransactions() {
   setTransactions(data);
 }
 
-function handleInput(e) {  
-  setForm({...form, [e.target.name]: e.target.value});
-}
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const res = await fetch('http://localhost:4000/transaction',{ //creating a new transaction
-    method: 'POST',
-    body: JSON.stringify(form),
-    headers: {
-      'content-type': 'application/json',
-    }
-  })
-  if(res.ok) {
-    setForm(InitialForm)
-    fetchTransactions();
-  }
- 
-}
+
 
   return (
     <div>
       <AppBar />
 
-      <TransactionForm />
+      <TransactionForm  fetchTransactions={fetchTransactions}/>
 
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="number" 
-          name="amount"
-          value={form.amount}
-          onChange={handleInput}
-          placeholder="Enter transaction amount" 
-        />
-        <input 
-          type="text"
-          name="description"
-          value={form.description}
-          onChange={handleInput}
-          placeholder="Enter transaction detail"
-        />
-        <input 
-          type="date"  
-          name="date"
-          value={form.date}
-          onChange={handleInput}
-        />
-        <button 
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-
+      
       <br />
 
       <section>
