477bf4232b6f9def51e697f66e9ab9f0236e5649
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-18 23:27:53 +0530

    clean App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,11 @@
-import { useEffect, useState } from 'react';
 import AppBar from './components/AppBar';
-import TransactionForm from './components/TransactionForm';
-import TransactionsList from './components/TransactionsList';
-import { Container } from '@mui/material';
-
-
+import { Outlet } from 'react-router-dom';
 
 function App() {  
-
-const [transactions, setTransactions] = useState([]);
-const [editTransaction, setEditTransaction] = useState({});
-
-useEffect(() => {
-  fetchTransactions();
-}, []);
-
-async function fetchTransactions() {
-  const res = await fetch('http://localhost:4000/transaction') //fetching data from db. no method == get
-  const {data} = await res.json();
-  setTransactions(data);
-}
   return (
     <div>
-      {/* COMPONENTS */}
       <AppBar />
-
-      <Container>
-        <TransactionForm  
-          fetchTransactions={fetchTransactions}
-          editTransaction={editTransaction}
-        />
-        <TransactionsList 
-          transactions={transactions}
-          fetchTransactions={fetchTransactions}
-          setEditTransaction={setEditTransaction}
-        />
-      </Container>
-
+      <Outlet />   {/* components of the children */}    
     </div>
   );
 }
