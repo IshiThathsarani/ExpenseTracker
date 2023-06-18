ba48580e3d69126561bd29af6bdd1028363003cc
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-18 10:06:40 +0530

    remove unnecessary codes

--- client/src/components/TransactionsList.js
+++ client/src/components/TransactionsList.js
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 
 
 export default function TransactionsList({transactions}) {
