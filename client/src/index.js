eb9f20ee55d687c81413e8a375b68d5acb481aaf
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-20 10:53:26 +0530

    clean the code

--- client/src/index.js
+++ client/src/index.js
@@ -3,36 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './index.css'
-import { createBrowserRouter,RouterProvider,} from "react-router-dom";
-import App from './App';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import { RouterProvider,} from "react-router-dom";
+import router from './routes';
 
-const router = createBrowserRouter([
-  {
-    // path: "/",
-    element: <App />,
-    children:[   
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]
-  },
-  // {
-  //   path: "/login",
-  //   element: <Login />,
-  // },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
